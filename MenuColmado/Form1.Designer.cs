1b72a93da895c68736b8691a18a3e21bceaa4cab
AlanMenaMon <menamonteroalan@outlook.com>
2026-07-30 17:09:50 -1000

    Add files via upload

---
+++ MenuColmado/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace MenuColmado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboProducto = new System.Windows.Forms.ComboBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbEfectivo = new System.Windows.Forms.RadioButton();
+            this.rbTarjeta = new System.Windows.Forms.RadioButton();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.830189F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(318, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Productos";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(325, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cantidad";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // cboProducto
+            // 
+            this.cboProducto.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboProducto.FormattingEnabled = true;
+            this.cboProducto.Location = new System.Drawing.Point(261, 57);
+            this.cboProducto.Name = "cboProducto";
+            this.cboProducto.Size = new System.Drawing.Size(183, 21);
+            this.cboProducto.TabIndex = 2;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(299, 176);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidad.TabIndex = 3;
+            this.txtCantidad.TextChanged += new System.EventHandler(this.txtCantidad_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbTarjeta);
+            this.groupBox1.Controls.Add(this.rbEfectivo);
+            this.groupBox1.Location = new System.Drawing.Point(265, 218);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Forma de pago";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // rbEfectivo
+            // 
+            this.rbEfectivo.AutoSize = true;
+            this.rbEfectivo.Location = new System.Drawing.Point(6, 42);
+            this.rbEfectivo.Name = "rbEfectivo";
+            this.rbEfectivo.Size = new System.Drawing.Size(64, 17);
+            this.rbEfectivo.TabIndex = 5;
+            this.rbEfectivo.TabStop = true;
+            this.rbEfectivo.Text = "Efectivo";
+            this.rbEfectivo.UseVisualStyleBackColor = true;
+            // 
+            // rbTarjeta
+            // 
+            this.rbTarjeta.AutoSize = true;
+            this.rbTarjeta.Location = new System.Drawing.Point(6, 19);
+            this.rbTarjeta.Name = "rbTarjeta";
+            this.rbTarjeta.Size = new System.Drawing.Size(62, 17);
+            this.rbTarjeta.TabIndex = 6;
+            this.rbTarjeta.TabStop = true;
+            this.rbTarjeta.Text = "Targeta";
+            this.rbTarjeta.UseVisualStyleBackColor = true;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.30189F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(260, 334);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(196, 29);
+            this.lblTotal.TabIndex = 7;
+            this.lblTotal.Text = "Total: RD$0.00";
+            this.lblTotal.Click += new System.EventHandler(this.lblTotal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.cboProducto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboProducto;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbEfectivo;
+        private System.Windows.Forms.RadioButton rbTarjeta;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ MenuColmado/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MenuColmado
+{
+    public partial class Form1 : Form
+    {
+        // Lista de productos con su precio
+        private readonly (string Nombre, decimal Precio)[] productos = new (string, decimal)[]
+        {
+            ("Arroz (lb)", 35.00m),
+            ("Habichuelas (lb)", 60.00m),
+            ("Pollo (lb)", 120.00m),
+            ("Huevos (cartón)", 180.00m),
+            ("Leche (galón)", 220.00m),
+            ("Pan (unidad)", 25.00m),
+            ("Aceite (litro)", 150.00m),
+            ("Refresco (2L)", 90.00m)
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Aquí conectamos los eventos, sin tocar el diseñador
+            this.Load += Form1_Load;
+            cboProducto.SelectedIndexChanged += Controles_CambioValor;
+            txtCantidad.TextChanged += Controles_CambioValor;
+            rbEfectivo.CheckedChanged += Controles_CambioValor;
+            rbTarjeta.CheckedChanged += Controles_CambioValor;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Textos de las etiquetas (label1 y label2 ya existían vacíos/con texto por defecto)
+            label1.Text = "Producto:";
+            label2.Text = "Cantidad:";
+
+            // Llenar el ComboBox con los productos y precios
+            cboProducto.Items.Clear();
+            foreach (var p in productos)
+            {
+                cboProducto.Items.Add($"{p.Nombre} - RD${p.Precio:0.00}");
+            }
+            cboProducto.SelectedIndex = 0;
+
+            txtCantidad.Text = "1";
+            rbEfectivo.Checked = true;
+
+            CalcularTotal();
+        }
+
+        // Un solo método que reacciona a cualquier cambio (producto, cantidad o forma de pago)
+        private void Controles_CambioValor(object sender, EventArgs e)
+        {
+            CalcularTotal();
+        }
+
+        private void CalcularTotal()
+        {
+            if (cboProducto.SelectedIndex == -1) return;
+
+            if (!int.TryParse(txtCantidad.Text, out int cantidad) || cantidad <= 0)
+            {
+                lblTotal.Text = "Total: RD$0.00";
+                return;
+            }
+
+            decimal precio = productos[cboProducto.SelectedIndex].Precio;
+            decimal total = precio * cantidad;
+            string metodoPago = rbEfectivo.Checked ? "Efectivo" : "Tarjeta";
+
+            lblTotal.Text = $"Total: RD${total:0.00}  ({metodoPago})";
+        }
+
+        // Este método ya lo tenía el diseñador conectado a label1, lo dejamos vacío para que no dé error
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTotal_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCantidad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
